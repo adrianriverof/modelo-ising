2bc9b78a2dba023060507d000e7ebac14b2e6cc3
adrianriverof <adrianriverof@gmail.com>
2024-05-14 12:52:17 +0200

    funcion para guardar matriz de salida en un txt

--- pruebas ej 1/gen_inicial_refactor.c
+++ pruebas ej 1/gen_inicial_refactor.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
+// Definición de la función para guardar la matriz en un archivo de texto
+void guardarMatrizEnArchivo(int **matriz, int n) {
+    FILE *archivo;
+    archivo = fopen("matriz.txt", "w"); // Abre el archivo para escritura
+
+    if (archivo != NULL) {
+        
+        // Escribe los elementos de la matriz en el archivo
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                fprintf(archivo, "%d ", matriz[i][j]);
+            }
+            fprintf(archivo, "\n");
+        }
+
+        fclose(archivo); // Cierra el archivo
+        printf("Matriz guardada en el archivo 'matriz.txt'.\n");
+    } else {
+        printf("Error al abrir el archivo.\n");
+    }
+}
+
+
+
 // Función para inicializar la matriz con números aleatorios
 void inicializarMatriz(int **matriz, int L) {
     srand(time(NULL)); // Inicializa la semilla aleatoria
@@ -50,6 +76,8 @@ int main() {
 
     // Imprimir la matriz
     imprimirMatriz(matriz, L);
+
+    guardarMatrizEnArchivo(matriz, L);
 
     // Liberar la memoria asignada dinámicamente
     for (int i = 0; i < L; i++) {
